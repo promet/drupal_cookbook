f5394c99627c117b9611d91a422b0c54694f8c9a
Will Milton <wa.milton@gmail.com>
2013-11-13 00:37:45 -0600

    simpler code in the provider, readability

--- providers/site.rb
+++ providers/site.rb
@@ -1,35 +1,39 @@
 action :create do
-
-  group = new_resource.group.nil? ? node['apache']['group'] : new_resource.group
-  owner = new_resource.owner.nil? ? node['apache']['user'] : new_resource.owner
-  uri       = new_resource.uri
-  doc_root  = new_resource.doc_root ? ::File.join(new_resource.root, new_resource.doc_root) : new_resource.root
-  site_path = "#{doc_root}/sites/#{new_resource.subdir}"
-  settings_path = new_resource.settings_dir.nil? ?  "/etc/drupals/#{uri}" : new_resource.settings_dir
+  group = @new_resource.group || node['apache']['group']
+  owner = @new_resource.owner || node['apache']['user']
+  settings_path = @new_resource.settings_dir || "/etc/drupals/#{@new_resource.uri}"
+  if @new_resource.doc_root
+    doc_root = ::File.join(@new_resource.root, @new_resource.doc_root)
+  else
+    doc_root = @new_resource.root
+  end
+  site_path = "#{doc_root}/sites/#{@new_resource.subdir}"
 
   directory settings_path do
-    owner     new_resource.owner
+    owner     owner
     group     group
     mode      0750
     recursive true
   end
 
   directory site_path do
-    owner     new_resource.owner
+    owner     owner
     group     group
     mode      0775
+    recursive true
   end
 
+  source = @new_resource.settings_source
   template "#{site_path}/settings.php" do
     action    :create_if_missing
-    owner     new_resource.owner
-    group     new_resource.group
+    owner     owner
+    group     group
     mode      0660
-    if new_resource.settings_source.nil?
+    if source.nil?
       source    "settings.php.erb"
       cookbook  'drupal'
     else
-      source new_resource.settings_source
+      source source
     end
       variables({
         username:       new_resource.db_username,
@@ -40,14 +44,15 @@ action :create do
   end
 
   settings_compile settings_path
-  web_app new_resource.uri do
+
+  uri = @new_resource.uri
+  web_app uri do
     server_name     uri
     docroot         doc_root
     server_aliases  []
     cookbook        'apache2'
     allow_override  ['All']
   end
-
   hostsfile_entry '127.0.1.1' do
     hostname  uri
     action    :append
@@ -62,11 +67,13 @@ def settings_compile(settings_path)
   site_conf_d     = "#{settings_path}/settings.conf.d"
   ini_conf_d      = "#{site_conf_d}/ini.conf.d"
   globals_conf_d  = "#{site_conf_d}/globals.conf.d"
-
+  owner = @new_resource.owner
+  group = @new_resource.group
+  config = @new_resource.config
   [site_conf_d, ini_conf_d, globals_conf_d].each do |dir|
     directory dir do
-      owner     new_resource.owner
-      group     new_resource.group
+      owner     owner
+      group     group
       recursive true
     end
   end
