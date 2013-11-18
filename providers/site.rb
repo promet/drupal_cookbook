4f362d138c283619fee7a8372b50a1f91cd71a9f
Will Milton <wa.milton@gmail.com>
2013-11-17 21:46:07 -0600

    just a config hash for our site config, no templating settings.php

--- providers/site.rb
+++ providers/site.rb
@@ -20,24 +20,23 @@ action :create do
     mode      0775
   end
 
-  template "#{site_path}/settings.php" do
-    action    :create_if_missing
-    owner     new_resource.owner
-    group     new_resource.group
-    mode      0660
-    if new_resource.settings_source.nil?
-      source    "settings.php.erb"
-      cookbook  'drupal'
-    else
-      source new_resource.settings_source
-    end
-      variables({
-        username:       new_resource.db_username,
-        password:       new_resource.db_password,
-        database:       new_resource.db,
-        settings_path:  settings_path,
-      })
+  cookbook_file 'settings.php' do
+    action  :create_if_missing
+    owner   owner
+    group   group
+    mode    0660
+    path    ::File.join(site_path, 'settings.php')
+    source  'settings.php'
   end
+
+  file ::File.join(site_path, 'env.json') {
+    owner   owner
+    group   group
+    content ::JSON.pretty_generate({
+      conf_dir: settings_path
+    })
+    mode    0660
+  }
 
   settings_compile settings_path
   web_app new_resource.uri do
@@ -59,63 +58,20 @@ action :create do
 end
 
 def settings_compile(settings_path)
-  site_conf_d     = "#{settings_path}/settings.conf.d"
-  ini_conf_d      = "#{site_conf_d}/ini.conf.d"
-  globals_conf_d  = "#{site_conf_d}/globals.conf.d"
-
-  [site_conf_d, ini_conf_d, globals_conf_d].each do |dir|
+  ini_conf_d      = "#{settings_path}/ini.conf.d"
+  globals_conf_d  = "#{settings_path}/globals.conf.d"
+  config = new_resource.config
+  [ini_conf_d, globals_conf_d].each do |dir|
     directory dir do
       owner     new_resource.owner
       group     new_resource.group
       recursive true
     end
   end
-
-  # TODO: break this out into its own LWRP
-  template "#{globals_conf_d}/databases.json" do
-    owner   new_resource.owner
-    group   new_resource.group
-    source  "databases.my.default.json.erb"
-    cookbook  'drupal'
+  file ::File.join(globals_conf_d, 'globals.default.json') do
+    owner   owner
+    group   group
+    content ::JSON.pretty_generate(config)
     mode    0660
-    variables({
-      username: new_resource.db_username,
-      password: new_resource.db_password,
-      database: new_resource.db,
-    })
-  end
-
-  template "#{globals_conf_d}/globals.default.json" do
-    owner   new_resource.owner
-    group   new_resource.group
-    cookbook  'drupal'
-    source  "globals.default.json.erb"
-    mode    0660
-    variables({
-      username: new_resource.db_username,
-      password: new_resource.db_password,
-      database: new_resource.db,
-    })
-  end
-
-end
-
-def mysql_init(user, pass, db)
-  mysql_connection = {
-    host:     'localhost',
-    username: 'root',
-    password: node['mysql']['server_root_password']
-  }
-
-  mysql_database db do
-    connection  mysql_connection
-    action      :create
-  end
-
-  mysql_database_user user do
-    connection    mysql_connection
-    password      pass
-    database_name db
-    action        [:create, :grant]
   end
 end
