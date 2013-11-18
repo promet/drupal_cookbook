eb7a421e86976b3ffff63eef936bbc8e472a4212
Will Milton <wa.milton@gmail.com>
2013-11-17 21:34:23 -0600

    removing useless specs, updating chefspec config

--- spec/default_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'chefspec'
-
-describe 'drupal::default' do
-  let (:chef_run) { ChefSpec::ChefRunner.new.converge 'drupal::default' }
-  it 'should do something' do
-    pending 'Your recipe examples go here.'
-  end
-end
