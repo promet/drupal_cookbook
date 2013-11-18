629b108420a77d483c73dda71f56760b3868bf68
Will Milton <wa.milton@gmail.com>
2013-11-17 22:20:17 -0600

    allowing use of data bags for sites

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,7 @@
-default['drupal']['drush']['root']    = '/etc/drushes'
-default['drupal']['drush']['version'] = '7.x-5.9'
+default.drupal.drush.root    = '/etc/drushes'
+default.drupal.drush.version = '7.x-5.9'
+default.drupal.sites_dir = '/var/www/sites'
+default.drupal.user = 'root'
+default.drupal.data_bag = 'drupal'
+default.drupal.data_bag_items = []
+default.drupal.shared_dir = '/etc/drupal/shared'
