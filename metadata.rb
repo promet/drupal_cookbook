8be7d63150d4caa8036f5d49f09b7c1d35f7aab5
Will Milton <wa.milton@gmail.com>
2013-11-17 22:13:42 -0600

    turns out, we were off by a spot when we named the release. yolo

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "wa.milton@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures some drupal projects"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.1"
+version          "1.2.0"
 
 depends "apache2"
 depends "ark"
