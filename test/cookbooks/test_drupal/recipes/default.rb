91ac9c4d21e4c42015cf393be65825c46a0cde9b
Will Milton <wa.milton@gmail.com>
2013-11-17 22:26:37 -0600

    testing with a kitchen

---
+++ test/cookbooks/test_drupal/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: test_drupal
+# Recipe:: default
+#
+# Copyright (C) 2013 YOUR_NAME
+# 
+# All rights reserved - Do Not Redistribute
+#
+
+node.override.build_essential.compiletime = true
+include_recipe 'build-essential'
